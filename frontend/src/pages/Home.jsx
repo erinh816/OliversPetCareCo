bf4a51711da16024aa5cbb5a9526ec1f0755e4fa
Silu Hu <erinh816@gmail.com>
2024-04-03 17:53:03 -0400

    added hero end section with staff, location and book buttons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import heroImage01 from "../assets/images/heroimg01.jpg";
 import heroImage02 from "../assets/images/heroimg02.jpg";
 import heroImage03 from "../assets/images/heroimg03.jpg";
+import icon01 from "../assets/images/icon1.webp";
+import icon02 from "../assets/images/icon2.webp";
+import icon03 from "../assets/images/icon3.webp";
+import { Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
 
 const Home = () => {
   return (
@@ -81,6 +86,54 @@ const Home = () => {
           Discover unparalleled pet care services tailored to your pets.
           </p>
         </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+          {/* first */}
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon01} alt=""/>
+            </div>
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Care Taker</h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">World class care for every pet.</p>
+              <Link to="/staff" className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                <BsArrowRight className="group-hover:text-white w-6 h-5"/>
+              </Link>
+            </div>
+          </div>
+
+          {/* second */}
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon02} alt=""/>
+            </div>
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Location</h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">We have 3 locations in Manhattan, Brookly and Westchester.</p>
+              <Link to="/staff" className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                <BsArrowRight className="group-hover:text-white w-6 h-5"/>
+              </Link>
+            </div>
+          </div>
+
+          {/* third */}
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon03} alt=""/>
+            </div>
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Book Appointment</h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">Check out available time slots!</p>
+              <Link to="/staff" className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                <BsArrowRight className="group-hover:text-white w-6 h-5"/>
+              </Link>
+            </div>
+          </div>
+
+
+
+        </div>
+
       </div>
     </section>
 
