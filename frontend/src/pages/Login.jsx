9c2dafc53b0c5281c246ee1dc9ca148a3de2ed19
Silu Hu <erinh816@gmail.com>
2024-06-14 17:02:52 -0400

    added form to login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,9 +4,17 @@ const Login = () => {
   return (
     <section className="px-5 lg:px-0">
       <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
-        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">Hello 
+        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">Hello! 
           <span className="text-primaryColor"> Welcome </span>Back
         </h3>
+        
+        {/* Login Form */}
+        <form className="py-4 mb:py-0">
+          <div className="mb-5">
+            <input type="email"></input>
+
+          </div>
+        </form>
 
       </div>
     </section>
